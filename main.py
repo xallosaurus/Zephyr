03ccc0fadfcdbd76b635dfc47e8b50dfd15d5e78
xallosaurus <xallosaurus2@outlook.com>
2025-03-27 23:15:07 -0300

    made scripts run on both mp4 files at once

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import ffmpeg
-import numpy
-import PIL
 import os
+import shutil
 
-filename = "lambo.mp4"
+# Create the ./output dir if it doesn't exist
+output_dir = './output'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir, exist_ok=True)
 
-os.mkdir(f"./output/{filename}")
+# Define video filenames
+filenames = ["lambo.mp4", "bronco.mp4"]
 
-ffmpeg.input(filename).filter("fps",fps=10).output(f'./output/{filename}/frame_%04d.png').run()
+for filename in filenames:
+    dir_path = f"{output_dir}/{filename}"
+    if os.path.exists(dir_path):
+        for img_name in os.listdir(dir_path):
+            os.remove(f"{dir_path}/{img_name}")
 
+    # Create the directories for each video
+    os.makedirs(dir_path, exist_ok=True)
+
+    # Run ffmpeg for each video
+    ffmpeg.input(filename).filter("fps", fps=10).output(f'{dir_path}/frame_%04d.png').run()
+
+print("Processing completed for all videos!")
--- turbulence.py
+++ turbulence.py
@@ -9,29 +9,23 @@ def absolute_file_paths(directory):
     return [entry.path for entry in os.scandir(path) if entry.is_file()]
 
 
-path = "./output/lambo.mp4"
-file_type = ".png"
-png_files = sorted (absolute_file_paths(path))
-#first_file = png_files_all[0]
-#file_count = len(png_files_all)
-#last_file = png_files_all [file_count - 1]
-#png_files = [first_file,last_file]
-
-
-
-averages = []
-
-for i in range(len(png_files)-1):
-    current_file = png_files[i]
-    next_file = png_files[i + 1]
-    image1 = Image.open(current_file)
-    image2 = Image.open(next_file)
-    gray_image1 = image1.convert("L")
-    gray_image2 = image2.convert("L")
-    grey_array1 = numpy.array(gray_image1)
-    grey_array2 = numpy.array(gray_image2)
-    abs_dif = numpy.abs(grey_array1 - grey_array2)
-    avg_dif = numpy.mean(abs_dif)
-    averages.append(avg_dif)
-average = numpy.max(averages)
-print (f"Average pixel varition across consecutive frames for {path} : {average}")
+paths = ["./output/bronco.mp4","./output/lambo.mp4"]
+for path in paths:
+    print(f"Starting work on png files in {path}")
+    file_type = ".png"
+    png_files = sorted (absolute_file_paths(path))
+    averages = []
+    for i in range(len(png_files)-1):
+        current_file = png_files[i]
+        next_file = png_files[i + 1]
+        image1 = Image.open(current_file)
+        image2 = Image.open(next_file)
+        gray_image1 = image1.convert("L")
+        gray_image2 = image2.convert("L")
+        grey_array1 = numpy.array(gray_image1)
+        grey_array2 = numpy.array(gray_image2)
+        abs_dif = numpy.abs(grey_array1 - grey_array2)
+        avg_dif = numpy.mean(abs_dif)
+        averages.append(avg_dif)
+    average = numpy.max(averages)
+    print (f"Average pixel varition across consecutive frames for {path} : {average}")
